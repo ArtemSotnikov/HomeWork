9266fbad7713fc178565a6db7e446410ef8b40b0
tasotnik <artem.sotnikov@hslu.ch>
2025-01-13 17:08:25 +0100

    Add empty index.html and script with variables

---
+++ home-work-18-bonus/office.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const officeNumber = "C312a";
+
+console.log(officeNumber);
+
+let numberOfDesks  = 15;
+
+console.log(numberOfDesks);
+
+const isLargeOffice = true;
+
+console.log(isLargeOffice);
+
+let largeTVSet = null;
+
+console.log(largeTVSet);
+
+let pictures = ["Mountains", "Sea", "Nature"]
+console.log(pictures);
+console.log(pictures[0]);
+
