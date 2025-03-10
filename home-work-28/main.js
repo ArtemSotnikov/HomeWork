fe5ea61e54c9db24025e659dafe26b5bd9d81805
tasotnik <artem.sotnikov@hslu.ch>
2025-03-10 20:47:36 +0100

    Add images to the duplicated slider (6 together). Increase number of grid columns. Seems to work fine, but need additional testing or some dynamic definition of grid-columns

--- home-work-28/main.js
+++ home-work-28/main.js
@@ -3,6 +3,6 @@ import {Slider} from "./Slider.js";
 const slider = new Slider("orig_slider");
 
 const sliderDuplicate = new Slider("dupl_slider",
-    ["assets/images/1.jpg", "assets/images/2.jpg", "assets/images/3.jpg", "assets/images/4.jpg", "assets/images/5.jpg"],
+    ["assets/images_add/1.jpg", "assets/images_add/2.jpg", "assets/images_add/3.jpg", "assets/images_add/4.jpg", "assets/images_add/5.jpg", "assets/images_add/6.jpg"],
     "red",
     "orange");
