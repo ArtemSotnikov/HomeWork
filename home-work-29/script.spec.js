2ef88694a8a01213c6a6975ad73c081b5453649c
tasotnik <artem.sotnikov@hslu.ch>
2025-03-13 20:12:38 +0100

    Implement jest test package for ageClassification.

--- home-work-29/script.spec.js
+++ home-work-29/script.spec.js
@@ -4,4 +4,36 @@ describe("Test ageClassification", () => {
     it("Should return null, when age not exist", () => {
         expect(ageClassification(-1)).toBe(null);
     })
+
+    it("Check childhood", () => {
+        expect(ageClassification(15)).toEqual("Дитинство");
+    })
+
+    it("Check if person young", () => {
+        expect(ageClassification(30) === "Молодість").toBeTruthy;
+    })
+
+    it("Check if person is adult", () => {
+        expect(ageClassification(45)).toMatch("Зрілість")
+    })
+
+    it("Check if person already quite old", () => {
+        expect(ageClassification(70)).toContain("Стар")
+    })
+
+    it("Check if person already old", () => {
+        expect(ageClassification(90)).toEqual("Довголіття")
+    })
+
+    it("Check if person very old", () => {
+        expect(ageClassification(Number("110"))).toEqual("Рекорд")
+    })
+
+    it("Should return null, when age is not realistic", () => {
+        expect(ageClassification(150)).toBeNull();
+    })
+
+    it("Check if person doesn't exist", () => {
+        expect(ageClassification("Dog")).toBeNull();
+    })
 })
