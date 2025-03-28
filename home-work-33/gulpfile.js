fb2f17783de33053409537f5bbab89b0d157b884
Artem <artemsotnikov@gmail.com>
2025-03-28 09:03:26 +0100

    Add translation to min.css, left commented code for normal .css

--- home-work-33/gulpfile.js
+++ home-work-33/gulpfile.js
@@ -1,10 +1,25 @@
 const { src, dest, task, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+const cssnano = require('cssnano');
+const rename = require('gulp-rename');
+const postcss = require('gulp-postcss');
 
-function scss() {
+//Leave it here to use it in further projects, if needed.
+//function scss() {
+//    return src('./src/**/*.scss')
+//        .pipe(sass().on('error', sass.logError))
+//        .pipe(dest('./assets/css'))
+//        .pipe(browserSync.stream());
+//}
+
+const plugins = [cssnano({ preset: 'default' })]
+
+function scssMin() {
     return src('./src/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(plugins))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(dest('./assets/css'))
         .pipe(browserSync.stream());
 }
@@ -24,10 +39,10 @@ function sync(done) {
 }
 
 function watchFiles(done) {
-    watch('./src/**/*.scss', scss)
+    watch('./src/**/*.scss', scssMin)
     watch('./*.html', sync)
     watch('./*.js', sync)
     done();
 }
 
-task('watch', series(scss, syncInit, watchFiles));
+task('watch', series(scssMin, syncInit, watchFiles));
