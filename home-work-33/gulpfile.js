5e84fba297dee9381f95ba4a7c0f8bf638c6665f
Artem <artemsotnikov@gmail.com>
2025-03-28 08:36:42 +0100

    Add browser update in real time

--- home-work-33/gulpfile.js
+++ home-work-33/gulpfile.js
@@ -1,10 +1,33 @@
-const { src, dest, task, watch } = require('gulp');
+const { src, dest, task, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
 
 function scss() {
     return src('./src/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(dest('./assets/css'))
+        .pipe(browserSync.stream());
 }
 
-task('scss', scss)
+function syncInit (done) {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    done();
+}
+
+function sync(done) {
+    browserSync.reload()
+    done();
+}
+
+function watchFiles(done) {
+    watch('./src/**/*.scss', scss)
+    watch('./*.html', sync)
+    watch('./*.js', sync)
+    done();
+}
+
+task('watch', series(scss, syncInit, watchFiles));
