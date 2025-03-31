63fab272936d6b52230f7b67de0c76e122c24f47
Artem <artemsotnikov@gmail.com>
2025-03-31 20:27:43 +0200

    Implement multi entries config for post and statistic

--- home-work-34/webpack.config.js
+++ home-work-34/webpack.config.js
@@ -2,9 +2,12 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        main: './src/index.js',
+        stat: './src/statistics.js'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
 };
