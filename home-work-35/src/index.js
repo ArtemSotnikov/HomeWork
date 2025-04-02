cbfa1acfac427942a6c39e3e671ba059b5b7e845
Artem <artemsotnikov@gmail.com>
2025-04-02 19:29:03 +0200

    Copy home work 34 to home work 35 install required packages to get state of hm34.

---
+++ home-work-35/src/index.js
@@ -0,0 +1,16 @@
+import Post from './post'
+import './css/style.css';
+import icon_square_big from './assets/images/icon-square-big.png';
+import moment from 'moment';
+
+const logoDiv = document.querySelector('.logo');
+const img = document.createElement('img');
+img.src = icon_square_big;
+img.alt = 'Logo';
+img.setAttribute("width", "200px");
+img.setAttribute("height", "200px");
+logoDiv.appendChild(img);
+
+
+const now = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log('Current time:', now);
---
+++ home-work-35/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/index.js',
+        stat: './src/statistics.js'
+    },
+    output: {
+        filename: '[name].bundle.[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif|webp)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[name].[hash][ext]',
+                }
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name].[hash][ext]'
+                }
+            }
+        ]
+    },
+    externals: {
+        moment: 'moment'
+    }
+};
