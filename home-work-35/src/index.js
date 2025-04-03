17b5bb3682a599a9e93f73b676bb6b56a229a24a
Artem <artemsotnikov@gmail.com>
2025-04-03 20:13:10 +0200

    Add Babel with config and test with arrow function

--- home-work-35/src/index.js
+++ home-work-35/src/index.js
@@ -11,6 +11,8 @@ img.setAttribute("width", "200px");
 img.setAttribute("height", "200px");
 logoDiv.appendChild(img);
 
-
 const now = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log('Current time:', now);
+
+const testBabel = () => console.log('Babel works!');
+testBabel();
