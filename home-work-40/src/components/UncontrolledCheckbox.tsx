7b9b80f23b434abb5b399d7012a36c3e091e17b0
Artem <artemsotnikov@gmail.com>
2025-04-24 09:34:36 +0200

    Hadle empty and default input with handleCheck

--- home-work-40/src/components/UncontrolledCheckbox.tsx
+++ home-work-40/src/components/UncontrolledCheckbox.tsx
@@ -1,10 +1,28 @@
 import { useRef } from "react";
 
 export default function UncontrolledCheckbox() {
-    const checkboxRef = useRef<HTMLInputElement>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleCheck = () => {
+        const inputValue = inputRef.current?.value.trim();
+
+        if (!inputValue || inputValue === "Please type your message here") {
+            alert("Please check your input before submission");
+        }
+    }
 
     return (
         <>
+            <div>
+                <label htmlFor="uncontrolledCheckbox">Check text if not empty:</label>
+                <input type="checkbox"  onChange={handleCheck}/>
+                <br/>
+                <input
+                    type="text"
+                    name="message"
+                    ref={inputRef}
+                    placeholder="Please type your massage here"/>
+            </div>
         </>
     )
 }
