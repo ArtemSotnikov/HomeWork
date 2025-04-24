f3cf3922f11ff089024319497c3a3bd808ca1450
Artem <artemsotnikov@gmail.com>
2025-04-24 09:50:15 +0200

    Disable alert when checbox is already checked.

--- home-work-40/src/components/UncontrolledCheckbox.tsx
+++ home-work-40/src/components/UncontrolledCheckbox.tsx
@@ -2,9 +2,15 @@ import { useRef } from "react";
 
 export default function UncontrolledCheckbox() {
     const inputRef = useRef<HTMLInputElement>(null);
+    const checkRef = useRef<HTMLInputElement>(null);
 
     const handleCheck = () => {
         const inputValue = inputRef.current?.value.trim();
+        const isChecked = checkRef.current?.checked;
+
+        if (!isChecked) {
+            return;
+        }
 
         if (!inputValue || inputValue === "Please type your message here") {
             alert("Please check your input before submission");
@@ -15,7 +21,9 @@ export default function UncontrolledCheckbox() {
         <>
             <div>
                 <label htmlFor="uncontrolledCheckbox">Check text if not empty:</label>
-                <input type="checkbox"  onChange={handleCheck}/>
+                <input type="checkbox"
+                       ref={checkRef}
+                       onChange={handleCheck}/>
                 <br/>
                 <input
                     type="text"
