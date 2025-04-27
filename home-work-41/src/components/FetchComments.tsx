0672c2183e61640d4695ade81b947be38e8ef34d
Artem <artemsotnikov@gmail.com>
2025-04-27 17:18:52 +0200

    Change app to one comment which is refreshed (refetched based on id of comment)

--- home-work-41/src/components/FetchComments.tsx
+++ home-work-41/src/components/FetchComments.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect, ChangeEvent} from "react";
 import axios, {AxiosResponse} from "axios";
 import {ItemComment} from "../interfaces/ItemComment.interface.ts";
 import Comment from "../components/Comment";
@@ -7,11 +7,12 @@ function FetchComments() {
     const [comments, setComments] = useState<ItemComment[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string>("");
+    const [commentId, setCommentId] = useState<number>(1);
 
     useEffect(() => {
         const fetchComments = async () => {
             try {
-                const response: AxiosResponse<ItemComment[]> = await axios.get('https://jsonplaceholder.typicode.com/comments');
+                const response: AxiosResponse<ItemComment[]> = await axios.get(` https://jsonplaceholder.typicode.com/comments?id=${commentId}` );
                 setComments(response.data);
             } catch (err) {
                 setError((err as Error).message);
@@ -21,7 +22,7 @@ function FetchComments() {
         };
 
         fetchComments();
-    }, []);
+    }, [commentId]);
 
     // Render first load and Error, before starting with list of comments. In case this list is heavy.
     if (loading) return <div>Comments are being loaded...</div>;
@@ -29,7 +30,7 @@ function FetchComments() {
 
     return (
         <>
-            <label htmlFor="effectDemoFetchPosts">Comments:</label>
+            <label htmlFor="effectDemoFetchPosts">Comments for Post ID {commentId}:</label>
             <div>
                 {
                     comments.map((comment: ItemComment) =>
@@ -40,6 +41,24 @@ function FetchComments() {
 
                     )
                 }
+            </div>
+            <div>
+                <input
+                    type="number"
+                    min={1}
+                    max={100}
+                    value={commentId}
+                    onChange={(e : ChangeEvent<HTMLInputElement>) => {
+                        const valueId = Number(e.target.value);
+                        if (valueId < 1 ) {
+                            setCommentId(1);
+                        } else if (valueId >= 1 && valueId <= 100) {
+                            setCommentId(valueId);
+                        } else { // >100
+                            setCommentId(100);
+                        }
+                    }}
+                />
             </div>
         </>
     );
