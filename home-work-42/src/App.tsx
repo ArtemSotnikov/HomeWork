04b4ac8cc07b0f259d80cc9c69ab8c32af7e40e4
Artem <artemsotnikov@gmail.com>
2025-04-29 08:52:45 +0200

    Add router with three components

--- home-work-42/src/App.tsx
+++ home-work-42/src/App.tsx
@@ -2,15 +2,18 @@ import './App.css'
 import Home from "./Components/Home.tsx";
 import About from "./Components/About.tsx";
 import Contact from "./Components/Contact.tsx";
+import {BrowserRouter as Router, Route, Routes} from "react-router";
 
 function App() {
 
   return (
-    <>
-      <Home></Home>
-      <About></About>
-      <Contact></Contact>
-    </>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </Router>
   )
 }
 
