96e7e4a8dbf3c11b5668b6a7343381d54dcdeb07
Artem <artemsotnikov@gmail.com>
2025-04-29 08:38:28 +0200

    Add three pages

--- home-work-42/src/App.tsx
+++ home-work-42/src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Home from "./Components/Home.tsx";
+import About from "./Components/About.tsx";
+import Contact from "./Components/Contact.tsx";
 
 function App() {
 
   return (
     <>
+      <Home></Home>
+      <About></About>
+      <Contact></Contact>
     </>
   )
 }
---
+++ home-work-42/src/Components/Home.tsx
@@ -0,0 +1,10 @@
+
+
+function Home() {
+
+    return (
+        <div> It is home page </div>
+    );
+}
+
+export default Home;
