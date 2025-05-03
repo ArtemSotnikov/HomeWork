3bd8774321b7f0a7c943cfcc2a877d942b30f66c
Artem <artemsotnikov@gmail.com>
2025-05-03 15:08:49 +0200

    Create files for Redux infrastructure. Put ShoppingList in Provider

---
+++ home-work-44/src/redux/store.ts
@@ -0,0 +1,8 @@
+import {configureStore} from "@reduxjs/toolkit";
+import {shoppingListReducer} from "./reducers.ts";
+
+export const store = configureStore({
+    reducer: {
+        shoppingList : shoppingListReducer,
+    }
+});
