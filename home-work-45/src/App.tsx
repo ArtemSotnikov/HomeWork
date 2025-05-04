091f672195657c581ad8bc9f91cfc79c80be64fa
Artem <artemsotnikov@gmail.com>
2025-05-04 18:49:16 +0200

    Initialize Vite project on React with TS, install Redux libraries. Move ShoppingApp from HM43 with Context.

---
+++ home-work-45/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ShoppingList from "./components/ShoppingList.tsx";
+import ShoppingListContextProvider from "./components/ShoppingListContextProvider.tsx";
+
+function App() {
+
+  return (
+      <ShoppingListContextProvider>
+          <ShoppingList />
+      </ShoppingListContextProvider>
+  )
+}
+
+export default App
---
+++ home-work-45/src/lists/itemsData.ts
@@ -0,0 +1,37 @@
+import { ItemData } from "../interfaces/ItemData.interface.ts";
+
+export const itemsData: ItemData[] = [
+    {
+        "id": 1,
+        "name": "carrot",
+        "amount": 1,
+        "unit": "kg"
+    },
+    {
+        "id": 2,
+        "name": "beer",
+        "amount": 2,
+        "unit": "l"
+    },
+    {
+        "id": 3,
+        "name": "milk",
+        "amount": 1,
+        "unit": "l",
+        "bestBefore": "1.05.2025"
+    },
+    {
+        "id": 4,
+        "name": "bread",
+        "amount": 2,
+        "unit": "pcs",
+        "bestBefore": "24.04.2025"
+    },
+    {
+        "id": 5,
+        "name": "water",
+        "amount": 2,
+        "unit": "l",
+
+    },
+]
