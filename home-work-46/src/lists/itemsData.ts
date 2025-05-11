b64f2777d5132d2b949bbc8d6fc5f58e434c12e1
Artem <artemsotnikov@gmail.com>
2025-05-11 19:29:24 +0200

    Change items list in code

--- home-work-46/src/lists/itemsData.ts
+++ home-work-46/src/lists/itemsData.ts
@@ -3,33 +3,33 @@ import type {ItemData} from "../interfaces/ItemData.interface.ts";
 export const itemsData: ItemData[] = [
     {
         "id": 1,
-        "name": "carrot",
-        "amount": 1,
+        "name": "apples",
+        "amount": 5,
         "unit": "kg"
     },
     {
         "id": 2,
-        "name": "beer",
+        "name": "vermouth",
         "amount": 2,
         "unit": "l"
     },
     {
         "id": 3,
-        "name": "milk",
+        "name": "water",
         "amount": 1,
         "unit": "l",
         "bestBefore": "1.05.2025"
     },
     {
         "id": 4,
-        "name": "bread",
+        "name": "chocolate",
         "amount": 2,
         "unit": "pcs",
         "bestBefore": "24.04.2025"
     },
     {
         "id": 5,
-        "name": "water",
+        "name": "kwas",
         "amount": 2,
         "unit": "l",
 
