ed2fa977091df3d3747b9e7b50558b3a09f53c5e
Artem <artemsotnikov@gmail.com>
2025-06-22 14:08:57 +0200

    Change article by id from json to defined temptlate.

--- home-work-60/backend/src/server.js
+++ home-work-60/backend/src/server.js
@@ -11,20 +11,31 @@ app.get('/articles', (req, res) => {
     res.json(articles);
 });
 
-app.get('/articles/:id', (req, res) => {
+// app.get('/articles/:id', (req, res) => {
+//     const id = parseInt(req.params.id);
+//     const article = articles.find(article => article.id === id);
+//     if (!article) {
+//         res.status(404).send('No article found with id ' + id);
+//     } else {
+//         res.status(200).send(article);
+//     }
+// })
+
+app.get('/articles/:id', function (req, res)  {
     const id = parseInt(req.params.id);
     const article = articles.find(article => article.id === id);
     if (!article) {
         res.status(404).send('No article found with id ' + id);
     } else {
-        res.status(200).send(article);
+        const data = { title_page: "Article " + article.id, title_article: article.title, content: article.text };
+        res.render('page', data);
     }
 })
 
-app.get('/', function (req, res) {
-    const data = { title_page: 'Main', title_article: 'Test!', content: 'Article content' };
-    res.render('page', data);
-})
+// app.get('/', function (req, res) {
+//     const data = { title_page: 'Main', title_article: 'Test!', content: 'Article content' };
+//     res.render('page', data);
+// })
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
