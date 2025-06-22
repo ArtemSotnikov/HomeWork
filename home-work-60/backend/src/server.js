8574ece2436cb4a59511b36cfdda3f86b46c1855
Artem <artemsotnikov@gmail.com>
2025-06-22 14:16:11 +0200

    Add main route to be able to navigate to aricles.

--- home-work-60/backend/src/server.js
+++ home-work-60/backend/src/server.js
@@ -7,19 +7,13 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.get('/', function (req, res) {
+   res.send('<a href="/articles">Go to Articles</a>');
+})
+
 app.get('/articles', (req, res) => {
     res.json(articles);
 });
-
-// app.get('/articles/:id', (req, res) => {
-//     const id = parseInt(req.params.id);
-//     const article = articles.find(article => article.id === id);
-//     if (!article) {
-//         res.status(404).send('No article found with id ' + id);
-//     } else {
-//         res.status(200).send(article);
-//     }
-// })
 
 app.get('/articles/:id', function (req, res)  {
     const id = parseInt(req.params.id);
@@ -31,11 +25,6 @@ app.get('/articles/:id', function (req, res)  {
         res.render('page', data);
     }
 })
-
-// app.get('/', function (req, res) {
-//     const data = { title_page: 'Main', title_article: 'Test!', content: 'Article content' };
-//     res.render('page', data);
-// })
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
