2ea618158168ac5e820e5d1d216ded98f573c166
Artem <artemsotnikov@gmail.com>
2025-07-05 12:30:53 +0200

    Add emails to users data

--- home-work-63/data/users.js
+++ home-work-63/data/users.js
@@ -2,16 +2,19 @@ export const users = [
     {
         id: 1,
         username: 'Lgraham',
+        email: 'lgraham@gmail.com',
         password: 'LeanneGraham123',
     },
     {
         id: 2,
         username: 'Ehowell',
+        email: 'ehowell@gmail.com',
         password: 'howellerwin',
     },
     {
         id: 3,
         username: 'Cbauch',
+        email: 'cbauch@gmail.com',
         password: 'clementine1960',
     }
 ]
