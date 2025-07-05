7d89aba423654dc3074e744a8a1433ec9a301379
Artem <artemsotnikov@gmail.com>
2025-07-05 12:28:45 +0200

    Start express-session and initialize passport. Add users data.

--- home-work-63/src/server.js
+++ home-work-63/src/server.js
@@ -1,9 +1,19 @@
 import express from "express";
+import session from "express-session";
+import passport from "passport";
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.listen(PORT, () => {
